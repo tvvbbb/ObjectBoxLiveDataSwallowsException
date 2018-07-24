2389507e0d940752ba5a8984cd55baa50611fd3c
tvvbbb <tvvbbb@163.com>
2018-07-24 15:24:33 +0800

    about generics

--- app/src/main/java/com/tvvbbb/objectboxlivedataswallowsexception/Test.java
+++ app/src/main/java/com/tvvbbb/objectboxlivedataswallowsexception/Test.java
@@ -2,20 +2,24 @@ package com.tvvbbb.objectboxlivedataswallowsexception;
 
 import io.objectbox.annotation.Entity;
 import io.objectbox.annotation.Id;
+import io.objectbox.relation.ToOne;
 
 /**
  * Created by tvvbb on 2018/7/23.
  */
 @Entity
-public class Test {
+public class Test<T> {
 
     @Id
     long id;
 
     int i;
 
+    ToOne<T> t;
+
     public Test(int i) {
         this.i = i;
     }
+
 
 }
